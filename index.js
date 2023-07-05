1ed849041f070026e12534622522c9182dc65712
hthimz <aamadmirevolution@gmail.com>
2023-07-05 10:08:43 +0530

    [WIP]env variables updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+require("dotenv").config();
 const { createWorker } = require("tesseract.js");
 const sharp = require("sharp");
 const path = require("path");
@@ -9,11 +10,13 @@ const fs = require("fs");
 const cors = require("cors");
 
 const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://hthimz:rootuser@namastecluster.dbnxz77.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
+const db = process.env.DB;
+const db_collection_name = process.env.COLLECTION;
 const client = new MongoClient(uri);
 app.use(cors());
 
+// Upload endpoint
 app.post("/upload", upload.single("image"), async (req, res) => {
   try {
     const imagePath = req.file.path;
@@ -23,11 +26,15 @@ app.post("/upload", upload.single("image"), async (req, res) => {
       `${imageName}_converted.png`
     );
 
+    // Convert the uploaded image to PNG format
     await sharp(imagePath).toFormat("png").toFile(convertedImagePath);
 
+    // Initialize Tesseract worker
     const worker = await createWorker({
-      logger: (m) => console.log("Loggin worker", m.progress),
+      logger: (m) => console.log("Logging worker", m.progress),
     });
+
+    // Perform OCR on the converted image
     let extractedText;
     console.log("Extracted", extractedText);
     (async () => {
@@ -40,6 +47,8 @@ app.post("/upload", upload.single("image"), async (req, res) => {
       const { data } = await worker.recognize(convertedImagePath);
       extractedText = data?.text;
       await worker.terminate();
+
+      // Extract name and roll number from the OCR result
       const nameStartIndex = extractedText.indexOf("Name") + 4;
       const nameEndIndex = extractedText.indexOf(" RollNumber");
       const name = extractedText
@@ -56,7 +65,7 @@ app.post("/upload", upload.single("image"), async (req, res) => {
         .substring(rollNumberStartIndex, rollNumberEndIndex)
         .trim();
 
-      // Extract subjects and marks
+      // Extract subjects and marks from the OCR result
       const subjectsStartIndex = extractedText.indexOf("Subjects Marks") + 14;
       const subjectsEndIndex = extractedText.length;
       const subjectsMarksText = extractedText
@@ -73,21 +82,30 @@ app.post("/upload", upload.single("image"), async (req, res) => {
           subjects.push({ subject: formattedSubject, marks: parseInt(marks) });
         }
       });
+
       const result = {
         name,
         rollNumber,
         subjects,
       };
-
-      await client.connect();
-      const collection = client.db("kyro").collection("marksheet");
-      const insertResult = await collection.insertOne(result);
-      console.log("Data inserted into MongoDB:", insertResult.insertedId);
-
       // Delete the uploaded file and converted PNG file
       fs.unlinkSync(imagePath);
       fs.unlinkSync(convertedImagePath);
       console.log("Files deleted");
+      await client.connect();
+      const collection = client.db(db).collection(db_collection_name);
+
+      // Check if the marksheet already exists in the database
+      const existingMarkSheet = await collection.findOne({ rollNumber });
+      if (existingMarkSheet) {
+        console.log("Marksheet already exists in the database");
+        res.status(409).json({ error: "Marksheet already exists" });
+        return;
+      }
+
+      // Insert the marksheet into the database
+      const insertResult = await collection.insertOne(result);
+      console.log("Data inserted into MongoDB:", insertResult.insertedId);
 
       console.log(JSON.stringify(result, null, 2));
       res.json(result);
@@ -98,11 +116,12 @@ app.post("/upload", upload.single("image"), async (req, res) => {
   }
 });
 
+// Get marksheet by roll number
 app.get("/students/:rollNumber", async (req, res) => {
   try {
     const rollNumber = req.params.rollNumber;
     await client.connect();
-    const collection = client.db("kyro").collection("marksheet");
+    const collection = client.db(db).collection(db_collection_name);
 
     const result = await collection.findOne({ rollNumber });
     if (result) {
@@ -115,25 +134,6 @@ app.get("/students/:rollNumber", async (req, res) => {
     res.status(500).json({ error: "Failed to retrieve the record" });
   }
 });
-
-const createRollNumberIndex = async () => {
-  try {
-    await client.connect();
-    const collection = client.db("kyro").collection("marksheet");
-
-    const indexKey = { rollNumber: 1 }; // Create an index on the rollNumber field in ascending order
-    const options = { unique: true }; // Set the unique option to true if you want rollNumber values to be unique
-
-    await collection.createIndex(indexKey, options);
-    console.log("Index created successfully");
-  } catch (error) {
-    console.error("Error:", error);
-  } finally {
-    await client.close();
-  }
-};
-
-// createRollNumberIndex(); // called to create Indexes based on rollNumber
 
 const port = 8000;
 app.listen(port, () => {
